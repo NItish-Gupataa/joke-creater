dc6a7c7f26e386c9b400986088afb18e800fcb45
Nitish-Guptaa <nitishkumar845401mth@gmail.com>
2023-09-28 20:01:12 +0530

    second commit

--- first.js
+++ first.js
@@ -1,1 +1,103 @@
-console.log("JavaScript")
+// const prompt=require("prompt-sync")();
+let arr=["1. Q: How do all the oceans say hello to each other? They wave!",
+"2. Q: What did one wall say to the other wall? I will meet you at the corner!",
+    
+    "3. What do you call a bear with no teeth? A gummy bear!",
+    
+    "4. Q: What do you call cheese that isnot yours? Nacho cheese!",
+    
+    "5. Q: Where do cows go for entertainment? To the moo-vies!",
+    
+    "6. Knock, knock. Who is there? Cows go. Cows go who? No, cows go MOO!",
+    
+    "7. Q:  What do you call a cow with no legs? Ground beef!",
+    
+    "8. Q: What do you call a cow with two legs? Lean meat!",
+    
+    "9. Q:  What do you call a pig that knows karate? A pork chop!",
+    
+    "10. Q: Why are ghosts bad liars? Because you can see right through them!",
+    
+    "11. Q: What animal needs to wear a wig? A bald eagle!",
+    
+    "12. Q: What do you call a fly without wings? A walk!",
+    
+    "13. Knock knock. Who is there? A little old lady? A little old lady who? I didn’t know you could yodel!",
+    
+    "14. Q: Why do bees have sticky hair? Because they use honey combs!",
+    
+    
+    "15. Q: What do you call an alligator in a vest? An investigator!",
+    
+    "16. Q:  Why canot you give Elsa a balloon? Because she will let it go!",
+    
+    "17. Q: What do you get when you cross a snowman with a vampire Frostbite!",
+    
+    "18. Q:  What has four wheels and flies? A garbage truck!",
+    
+    "19. Q:  Why did the man run around his bed? Because he was trying to catch up on his sleep!",
+    
+    "20. Q:  Why did the math book look so sad? Because it had so many problems!",
+    
+    "21. Q:  Can a kangaroo jump higher than the Empire State Building? Of course!  The Empire State Building can’t jump!",
+    
+    "22. Q:  If April showers bring Mayflowers, what do Mayflowers bring?Pilgrims!",
+    
+    "23. Q: What do you call a sleeping bull? A bulldozer!",
+    
+    "24. Q: What did the zero say to the eight? Nice belt!",
+    
+    "25. Q:  Why do sharks swim in saltwater? Because pepper water makes them sneeze!",
+    
+    "26. Q: Where do you find a dog with no legs? Right where you left him!",
+    
+    "27. Q:  Where do fish keep their money? In the river bank!",
+    
+    "28. Q:  Why did the gum cross the road? It was stuck to the chickens foot!",
+    
+    "29. Q:  What is brown and sticky? A stick!",
+    
+    "30. Q:  Why did the picture go to jail? It was framed!",
+    
+    "31. Q: How do you know if there is an elephant under your bed? Your head hits the ceiling!",
+    
+    "32. Q: Why are elephants so wrinkled? Because they take too long to iron!",
+    
+    "33. Q: How do you keep an elephant from charging? Take away her credit card!",
+    
+    "34. Q:  Why did the elephant paint himself different colors? So he could hide in the crayon box!",
+    
+    "35. Q:  How can you tell if an elephant has been in your refrigerator?By the footprints in the butter!",
+    
+    "36. Q: What is the difference between elephants and grapes? Grapes are purple.",
+    
+    "37. Q:  What did Tarzan say when he saw the elephants coming? “Here come the elephants!”",
+    
+    "38. Q:  What did Jane say when she saw the elephants coming? “Here come the grapes!” (She was colorblind.)",
+    
+    "39. Q:  Why did the chicken cross the playground? To get to the other slide!",
+    
+    
+    "40. Q:  What can you catch but not throw? A cold!",
+    
+    "41. Q:  What has hands but canot clap? A clock!",
+    
+    "42. Q: What do you call a dog that can tell time? A watch dog!",
+    
+    "43. Q:  What did one hat say to the other? Stay here, I am going on ahead. (going on a head)",
+    
+    "44. Q:  What side of a turkey has the most feathers? The outside!",
+    
+    "45. Q:  What falls in winter but never gets hurt? The snow!",
+    
+    "46. Q:  Why did the teacher put on sunglasses? Because her students were so bright! ",
+    
+    "47. Q: How did Benjamin Franklin feel when he discovered electricity? Shocked!",
+    
+    "48. Q:  Why do strings never win a race? Because they always tie!",
+    
+    "49. Q:  What kind of shoes do ninjas wear? Sneakers!",
+    
+    "50. Q: What do you call a flower that runs on electricity? A power plant!"]
+    let index=Math.floor(Math.random()*(arr.length-1))
+    console.log(index)
